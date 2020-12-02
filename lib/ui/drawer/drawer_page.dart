d916454df6741fa3bf55f4489cafc993a5fa4f4f
blendthink <info@blendthink.dev>
2020-12-02 23:15:32 +0900

    ドロワーページを仮作成

---
+++ lib/ui/drawer/drawer_page.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class DrawerPage extends StatelessWidget {
+  const DrawerPage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        children: [
+          // TODO(okayama): 後から適切なものに修正する
+          ListTile(
+            leading: const Icon(Icons.web),
+            title: Text(
+              'URL',
+              style: Theme.of(context).textTheme.subtitle1,
+            ),
+            onTap: () {
+              // TODO(okayama): URL ページに遷移する
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.tag),
+            title: Text(
+              'Tag',
+              style: Theme.of(context).textTheme.subtitle1,
+            ),
+            onTap: () {
+              // TODO(okayama): Tag ページに遷移する
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
