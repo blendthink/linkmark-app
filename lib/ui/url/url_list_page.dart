0df00bfc4103db0a26a98335db26e1ed869850e9
blendthink <info@blendthink.dev>
2020-12-03 01:26:33 +0900

    inputchip の部分を仮実装

--- lib/ui/url/url_list_page.dart
+++ lib/ui/url/url_list_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:linkmark_app/ui/drawer/drawer_page.dart';
 
 const urls = [
+  'https://qiita.com/popy1017/items/732229abc14e73933269',
   'http://google.com',
   'http://twitter.com',
 ];
@@ -13,15 +14,29 @@ class UrlListPage extends StatelessWidget {
       drawer: const DrawerPage(),
       appBar: AppBar(
         title: const Text('Linkmark'),
+        elevation: 0,
       ),
-      body: ListView(
-        children: urls.map(
-          (url) {
-            return ListTile(
-              title: Text(url),
-            );
-          },
-        ).toList(),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Ink(
+            color: Theme.of(context).primaryColor,
+            child: SizedBox(
+              height: 60,
+            ),
+          ),
+          Expanded(
+            child: ListView(
+              children: urls.map(
+                    (url) {
+                  return ListTile(
+                    title: Text(url),
+                  );
+                },
+              ).toList(),
+            ),
+          ),
+        ],
       ),
     );
   }
