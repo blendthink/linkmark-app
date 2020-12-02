98b3aee019d49235fb5c24f0d18d717b50a24636
blendthink <info@blendthink.dev>
2020-12-03 00:24:07 +0900

    タイトルと仮のURLデータを作成

--- lib/ui/url/url_list_page.dart
+++ lib/ui/url/url_list_page.dart
@@ -1,12 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:linkmark_app/ui/drawer/drawer_page.dart';
 
+const urls = [
+  'http://google.com',
+  'http://twitter.com',
+];
+
 class UrlListPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       drawer: const DrawerPage(),
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: const Text('Linkmark'),
+      ),
+      body: ListView(
+        children: urls.map(
+          (url) {
+            return ListTile(
+              title: Text(url),
+            );
+          },
+        ).toList(),
+      ),
     );
   }
 }
