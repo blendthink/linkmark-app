69728b50eb0bf972179053a26ecb78dfbb0734e5
blendthink <info@blendthink.dev>
2020-12-02 23:37:23 +0900

    main, app の基盤を整理

---
+++ lib/app.dart
@@ -0,0 +1,21 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:get/get.dart';
+import 'package:linkmark_app/constants.dart';
+import 'package:linkmark_app/ui/url/url_list_page.dart';
+
+class App extends HookWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: 'Linkmark',
+      theme: ThemeData(),
+      darkTheme: ThemeData.dark(),
+      home: UrlListPage(),
+      routes: {
+        Constants.pageUrlList: (context) => UrlListPage(),
+      },
+    );
+  }
+}
