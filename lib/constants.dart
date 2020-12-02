e5c70e3fc8cb5fd56d3476ac1567599c7abb3d84
Tatsuya Okayama <t_okayama@yumemi.co.jp>
2020-12-02 22:58:27 +0900

    定数を管理するファイルを作成

---
+++ lib/constants.dart
@@ -0,0 +1,46 @@
+import 'package:enum_to_string/enum_to_string.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+enum Flavor { development, production }
+
+@immutable
+class Constants {
+  const Constants();
+
+  factory Constants.of() {
+    if (_instance != null) {
+      return _instance;
+    }
+
+    final flavor = EnumToString.fromString(
+      Flavor.values,
+      const String.fromEnvironment('FLAVOR'),
+    );
+
+    switch (flavor) {
+      case Flavor.development:
+        _instance = Constants._dev();
+        break;
+      case Flavor.production:
+      default:
+        _instance = Constants._prd();
+    }
+    return _instance;
+  }
+
+  factory Constants._dev() {
+    return const Constants();
+  }
+
+  factory Constants._prd() {
+    return const Constants();
+  }
+
+  // Routing name
+  static const String pageHome = '/home';
+
+  static const bool isDebugMode = kDebugMode;
+
+  static Constants _instance;
+}
