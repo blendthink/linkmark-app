1e833de8913087ebd68041d2400d64da3ad11b01
blendthink <info@blendthink.dev>
2020-12-07 22:55:29 +0900

    途中の状態をコミット。構成を見直す必要あり。

---
+++ lib/ui/component/appbar/tag_filter_widget.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+const tags = [
+  'Android',
+  'iOS',
+  'Mobile',
+];
+
+class TagFilterWidget extends StatefulWidget implements PreferredSizeWidget {
+  @override
+  Size get preferredSize => Size.fromHeight(56.0);
+
+  @override
+  State<StatefulWidget> createState() => _TagFilterState();
+}
+
+class _TagFilterState extends State<TagFilterWidget> {
+
+  List<bool> _listSelected;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _listSelected = tags.map((_) => false).toList();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    return SizedBox(
+      height: 56.0,
+      child: ListView.builder(
+        itemCount: tags.length,
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (context, index) {
+          final tag = tags[index];
+          return FilterChip(
+            selected: _listSelected[index],
+            selectedColor: Theme.of(context).primaryColor,
+            showCheckmark: false,
+            label: Text(tag),
+            onSelected: (bool value) {
+              setState(() {
+                _listSelected[index] = value;
+              });
+            },
+          );
+        },
+      ),
+    );
+  }
+}
