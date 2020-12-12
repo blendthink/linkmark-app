ee4af7d012f7e810385d281ad1cf678e9d8cb97b
Tatsuya Okayama <t_okayama@yumemi.co.jp>
2020-12-13 06:03:06 +0900

    URL のバリデーションを実装

--- lib/ui/page/link/edit_page.dart
+++ lib/ui/page/link/edit_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:validators/validators.dart';
 
 class EditPage extends StatelessWidget {
   void _close(BuildContext context) {
@@ -7,27 +8,52 @@ class EditPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(
+
+    final appBar = AppBar(
+      leading: IconButton(
+        onPressed: () {
+          _close(context);
+        },
+        icon: Icon(Icons.close),
+      ),
+      title: Text('Edit Link'),
+      actions: [
+        IconButton(
           onPressed: () {
             _close(context);
           },
-          icon: Icon(Icons.close),
+          icon: Icon(Icons.check),
         ),
-        title: Text('Edit Link'),
-        actions: [
-          IconButton(
-            onPressed: () {
-              _close(context);
-            },
-            icon: Icon(Icons.check),
-          ),
-        ],
+      ],
+    );
+
+    final urlForm = Padding(
+      padding: const EdgeInsets.all(16),
+      child: TextFormField(
+        decoration: const InputDecoration(
+          icon: Icon(Icons.link),
+          border: OutlineInputBorder(),
+          filled: true,
+          labelText: "Enter URL",
+          hintText: 'https://',
+        ),
+        autovalidateMode: AutovalidateMode.onUserInteraction,
+        validator: (value) {
+          return !isURL(value, allowUnderscore: true)
+              ? 'Please enter URL'
+              : null;
+        },
       ),
+    );
+
+    return Scaffold(
+      appBar: appBar,
       body: Container(
-        height: double.infinity,
-        width: double.infinity,
+        child: Column(
+          children: [
+            urlForm,
+          ],
+        ),
       ),
     );
   }
