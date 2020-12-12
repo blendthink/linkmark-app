141e6f9fe375cb870c3cb837ee931eaf0ec0614c
Tatsuya Okayama <t_okayama@yumemi.co.jp>
2020-12-13 05:13:31 +0900

    url のパッケージを link にへんこう

--- lib/app.dart
+++ lib/app.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:get/get.dart';
 import 'package:linkmark_app/constants.dart';
-import 'package:linkmark_app/ui/page/url/url_list_page.dart';
+import 'package:linkmark_app/ui/page/link/url_list_page.dart';
 
 class App extends HookWidget {
   @override
