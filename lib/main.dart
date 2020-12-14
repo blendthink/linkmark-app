ec6cf7a8407cf8813c69f520929b7077bb2355bf
Tatsuya Okayama <t_okayama@yumemi.co.jp>
2020-12-14 11:07:23 +0900

    ios の Firebase 設定

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,29 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 import 'dart:async';
 
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:linkmark_app/app.dart';
+import 'package:linkmark_app/constants.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
+  // Firebase
+  await Firebase.initializeApp();
+  // Crashlytics
+  await FirebaseCrashlytics.instance
+      .setCrashlyticsCollectionEnabled(Constants.isDebugMode);
+  Function originalOnError = FlutterError.onError;
+  FlutterError.onError = (errorDetails) async {
+    await FirebaseCrashlytics.instance.recordFlutterError(errorDetails);
+    originalOnError(errorDetails);
+  };
+
   runZonedGuarded(() {
     runApp(ProviderScope(child: App()));
   }, (error, stackTrace) {
-    // TODO(okayama): FirebaseCrashlytics とかに送る
+    FirebaseCrashlytics.instance.recordError(error, stackTrace);
   });
 }
