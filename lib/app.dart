d32684c2dba943200b9f46398726d1db23a2c31c
Tatsuya Okayama <t_okayama@yumemi.co.jp>
2020-12-16 21:59:46 +0900

    ログイン成功するところまではいけた

--- lib/app.dart
+++ lib/app.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:get/get.dart';
 import 'package:linkmark_app/constants.dart';
+import 'package:linkmark_app/ui/page/auth_page.dart';
 import 'package:linkmark_app/ui/page/link/index_page.dart';
 
 class App extends HookWidget {
@@ -12,7 +13,7 @@ class App extends HookWidget {
       title: 'Linkmark',
       theme: ThemeData(),
       darkTheme: ThemeData.dark(),
-      home: IndexPage(),
+      home: AuthPage(),
       routes: {
         Constants.pageUrlList: (context) => IndexPage(),
       },
