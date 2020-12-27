7d85993a4c90d148a0822e57da9c45ffc8738208
Tatsuya Okayama <t_okayama@yumemi.co.jp>
2020-12-27 20:25:53 +0900

    IndexViewModel で Links を取得できるようにした

--- lib/ui/page/link/index_view_model.dart
+++ lib/ui/page/link/index_view_model.dart
@@ -1,9 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:linkmark_app/data/model/link.dart';
+import 'package:linkmark_app/data/provider/links_repository_provider.dart';
+import 'package:linkmark_app/data/repository/links_repository.dart';
+import 'package:linkmark_app/data/model/result.dart';
 
-final indexViewModelProvider =
-    ChangeNotifierProvider((ref) => IndexViewModel());
+final indexViewModelProvider = ChangeNotifierProvider(
+    (ref) => IndexViewModel(ref.read(linksRepositoryProvider)));
 
 class IndexViewModel extends ChangeNotifier {
-  IndexViewModel();
+  IndexViewModel(this._repository);
+
+  final LinksRepository _repository;
+
+  Result<List<Link>> _links;
+
+  Result<List<Link>> get links => _links;
+
+  Future<void> fetchLinks() async {
+    return _repository
+        .getLinks()
+        .then((value) => _links = value)
+        .whenComplete(notifyListeners);
+  }
 }
