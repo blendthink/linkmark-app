d9c84671403531b513a25995ece690d40eda56fe
blendthink <info@blendthink.dev>
2020-12-29 21:56:15 +0900

    Shimmer の Widget をリファクタ

--- lib/ui/component/link/list/link_list_item_shimmer.dart
+++ lib/ui/component/link/list/link_list_item_shimmer.dart
@@ -5,32 +5,39 @@ import 'package:shimmer/shimmer.dart';
 class LinkListItemShimmer extends StatelessWidget {
   const LinkListItemShimmer({Key key}) : super(key: key);
 
+  static const _titleShimmerHeight = 12.0;
+  static const _titleVerticalGap = const Gap(4);
+
+  static const _subTitleShimmerHeight = 8.0;
+  static const _subTitleVerticalGap = const Gap(2);
+
+  static const _imageWidth = 80.0;
+  static const _imageHeight = 56.0;
+
   @override
   Widget build(BuildContext context) {
     final screenWidth = MediaQuery.of(context).size.width;
     final textWidth = screenWidth - (80 + 48);
 
-    const gapTextVerticalTitle = const Gap(4);
-    final containerInfinityWidthTitle = Container(
+    final titleContainerInfinityWidth = Container(
       width: double.infinity,
-      height: 12.0,
+      height: _titleShimmerHeight,
       color: Colors.white,
     );
-    final containerHalfwayWidthTitle = Container(
+    final titleContainerHalfwayWidth = Container(
       width: textWidth * 2 / 5,
-      height: 12.0,
+      height: _titleShimmerHeight,
       color: Colors.white,
     );
 
-    const gapTextVerticalSubTitle = const Gap(2);
-    final containerInfinityWidthSubTitle = Container(
+    final subTitleContainerInfinityWidth = Container(
       width: double.infinity,
-      height: 8.0,
+      height: _subTitleShimmerHeight,
       color: Colors.white,
     );
-    final containerHalfwayWidthSubTitle = Container(
+    final subTitleContainerHalfwayWidth = Container(
       width: textWidth / 5,
-      height: 8.0,
+      height: _subTitleShimmerHeight,
       color: Colors.white,
     );
 
@@ -38,7 +45,7 @@ class LinkListItemShimmer extends StatelessWidget {
       baseColor: Colors.grey[300],
       highlightColor: Colors.grey[100],
       child: Padding(
-        padding: const EdgeInsets.all(16.0),
+        padding: const EdgeInsets.all(16),
         child: Row(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
@@ -46,22 +53,22 @@ class LinkListItemShimmer extends StatelessWidget {
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
-                  containerInfinityWidthTitle,
-                  gapTextVerticalTitle,
-                  containerHalfwayWidthTitle,
-                  gapTextVerticalTitle,
-                  containerInfinityWidthSubTitle,
-                  gapTextVerticalSubTitle,
-                  containerInfinityWidthSubTitle,
-                  gapTextVerticalSubTitle,
-                  containerHalfwayWidthSubTitle,
+                  titleContainerInfinityWidth,
+                  _titleVerticalGap,
+                  titleContainerHalfwayWidth,
+                  _titleVerticalGap,
+                  subTitleContainerInfinityWidth,
+                  _subTitleVerticalGap,
+                  subTitleContainerInfinityWidth,
+                  _subTitleVerticalGap,
+                  subTitleContainerHalfwayWidth,
                 ],
               ),
             ),
             const Gap(16),
             Container(
-              width: 80.0,
-              height: 56.0,
+              width: _imageWidth,
+              height: _imageHeight,
               color: Colors.white,
             ),
           ],
