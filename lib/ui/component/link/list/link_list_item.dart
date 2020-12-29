16581ff61e4e5e6481bd855a946975a7b5a6c3d3
blendthink <info@blendthink.dev>
2020-12-29 21:23:42 +0900

    Link の ListItem の Widget を分解した

---
+++ lib/ui/component/link/list/link_list_item.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:linkmark_app/data/model/link.dart';
+import 'package:linkmark_app/ui/component/link/list/link_list_item_shimmer.dart';
+import 'package:linkmark_app/ui/page/link/index_view_model.dart';
+import 'package:linkmark_app/util/ext/async_snapshot.dart';
+import 'package:linkmark_app/util/ext/string.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class LinkListItem extends HookWidget {
+  LinkListItem({
+    this.linksMap,
+    this.index,
+    Key key,
+  }) : super(key: key);
+
+  final Map<String, Link> linksMap;
+  final int index;
+
+  _launchURL({@required String url}) async {
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      print('Could not launch $url');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final indexViewModel = context.read(indexViewModelProvider);
+
+    final linkMap = linksMap.entries.elementAt(index);
+    final link = linkMap.value;
+
+    final snapshotDetail = useFuture(useMemoized(() {
+      return indexViewModel.fetchLinkMetadata(index: index);
+    }, [link.toString()]));
+
+    Widget listTile;
+
+    if (!snapshotDetail.isDone) {
+      listTile = const LinkListItemShimmer();
+    } else {
+      listTile = ListTile(
+        title: Text(
+          link.title.trimNewline(),
+          maxLines: 2,
+          overflow: TextOverflow.ellipsis,
+          style: Theme.of(context).textTheme.subtitle2,
+        ),
+        subtitle: Text(
+          link.description.trimNewline(),
+          maxLines: 3,
+          overflow: TextOverflow.ellipsis,
+          style: Theme.of(context).textTheme.caption,
+        ),
+        trailing: link.imageUrl == null
+            ? null
+            : Image.network(
+                link.imageUrl,
+                width: 80,
+                height: 56,
+                fit: BoxFit.fitHeight,
+              ),
+        contentPadding: const EdgeInsets.symmetric(
+          vertical: 8,
+          horizontal: 16,
+        ),
+      );
+    }
+    return Card(
+      child: InkWell(
+        onTap: () {
+          _launchURL(url: link.url);
+        },
+        child: listTile,
+      ),
+    );
+  }
+}
--- lib/ui/page/link/index_page.dart
+++ lib/ui/page/link/index_page.dart
@@ -1,19 +1,15 @@
 import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:gap/gap.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:linkmark_app/data/model/link.dart';
 import 'package:linkmark_app/ui/component/container_with_loading.dart';
+import 'package:linkmark_app/ui/component/link/list/link_list_item.dart';
 import 'package:linkmark_app/ui/component/loading/loading_state_view_model.dart';
 import 'package:linkmark_app/ui/page/link/index_view_model.dart';
 import 'package:linkmark_app/util/ext/async_snapshot.dart';
-import 'package:linkmark_app/util/ext/string.dart';
 import 'package:flutter/material.dart';
 import 'package:linkmark_app/ui/component/appbar/search_app_bar.dart';
 import 'package:linkmark_app/ui/page/drawer/drawer_page.dart';
 import 'package:linkmark_app/ui/page/link/edit_page.dart';
 import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
-import 'package:shimmer/shimmer.dart';
-import 'package:url_launcher/url_launcher.dart';
 
 class IndexPage extends StatelessWidget {
   void _onTextChanged(String text) {
@@ -45,7 +41,7 @@ class IndexPage extends StatelessWidget {
           child: ListView.builder(
             itemCount: data.length,
             itemBuilder: (_, index) {
-              return LinkItem(
+              return LinkListItem(
                 linksMap: data,
                 index: index,
               );
@@ -73,138 +69,6 @@ class IndexPage extends StatelessWidget {
       ),
       body: ContainerWithLoading(
         child: hookBuilder,
-      ),
-    );
-  }
-}
-
-class LinkItem extends HookWidget {
-  LinkItem({
-    this.linksMap,
-    this.index,
-    Key key,
-  }) : super(key: key);
-
-  final Map<String, Link> linksMap;
-  final int index;
-
-  _launchURL({@required String url}) async {
-    if (await canLaunch(url)) {
-      await launch(url);
-    } else {
-      print('Could not launch $url');
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    final indexViewModel = context.read(indexViewModelProvider);
-
-    final linkMap = linksMap.entries.elementAt(index);
-    final link = linkMap.value;
-
-    final snapshotDetail = useFuture(useMemoized(() {
-      return indexViewModel.fetchLinkMetadata(index: index);
-    }, [link.toString()]));
-
-    Widget listTile;
-
-    if (!snapshotDetail.isDone) {
-      final screenWidth = MediaQuery.of(context).size.width;
-      final textWidth = screenWidth - (80 + 48);
-
-      const gapTextVerticalTitle = const Gap(4);
-      final containerInfinityWidthTitle = Container(
-        width: double.infinity,
-        height: 12.0,
-        color: Colors.white,
-      );
-      final containerHalfwayWidthTitle = Container(
-        width: textWidth * 2 / 5,
-        height: 12.0,
-        color: Colors.white,
-      );
-
-      const gapTextVerticalSubTitle = const Gap(2);
-      final containerInfinityWidthSubTitle = Container(
-        width: double.infinity,
-        height: 8.0,
-        color: Colors.white,
-      );
-      final containerHalfwayWidthSubTitle = Container(
-        width: textWidth / 5,
-        height: 8.0,
-        color: Colors.white,
-      );
-
-      listTile = Shimmer.fromColors(
-        baseColor: Colors.grey[300],
-        highlightColor: Colors.grey[100],
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    containerInfinityWidthTitle,
-                    gapTextVerticalTitle,
-                    containerHalfwayWidthTitle,
-                    gapTextVerticalTitle,
-                    containerInfinityWidthSubTitle,
-                    gapTextVerticalSubTitle,
-                    containerInfinityWidthSubTitle,
-                    gapTextVerticalSubTitle,
-                    containerHalfwayWidthSubTitle,
-                  ],
-                ),
-              ),
-              const Gap(16),
-              Container(
-                width: 80.0,
-                height: 56.0,
-                color: Colors.white,
-              ),
-            ],
-          ),
-        ),
-      );
-    } else {
-      listTile = ListTile(
-        title: Text(
-          link.title.trimNewline(),
-          maxLines: 2,
-          overflow: TextOverflow.ellipsis,
-          style: Theme.of(context).textTheme.subtitle2,
-        ),
-        subtitle: Text(
-          link.description.trimNewline(),
-          maxLines: 3,
-          overflow: TextOverflow.ellipsis,
-          style: Theme.of(context).textTheme.caption,
-        ),
-        trailing: link.imageUrl == null
-            ? null
-            : Image.network(
-                link.imageUrl,
-                width: 80,
-                height: 56,
-                fit: BoxFit.fitHeight,
-              ),
-        contentPadding: const EdgeInsets.symmetric(
-          vertical: 8,
-          horizontal: 16,
-        ),
-      );
-    }
-    return Card(
-      child: InkWell(
-        onTap: () {
-          _launchURL(url: link.url);
-        },
-        child: listTile,
       ),
     );
   }
