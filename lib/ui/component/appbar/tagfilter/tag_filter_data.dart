4efce95243a8b2268c2ee32959e84da32fef4957
blendthink <info@blendthink.dev>
2021-01-01 21:33:00 +0900

    Tag Filter の基盤を作成

---
+++ lib/ui/component/appbar/tagfilter/tag_filter_data.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/foundation.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:linkmark_app/data/model/tag.dart';
+
+part 'tag_filter_data.freezed.dart';
+part 'tag_filter_data.g.dart';
+
+@freezed
+abstract class TagFilterData with _$TagFilterData {
+  const factory TagFilterData({
+    @required bool selected,
+    @required Tag tag,
+  }) = _TagFilterData;
+
+  factory TagFilterData.fromJson(Map<String, dynamic> json) =>
+      _$TagFilterDataFromJson(json);
+}
