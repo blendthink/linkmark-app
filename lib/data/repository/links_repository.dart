2a0be076273c8628d8f167af1e6d448e2c1183ba
Tatsuya Okayama <t_okayama@yumemi.co.jp>
2021-01-04 20:34:16 +0900

    Link の create メソッドを作成

--- lib/data/repository/links_repository.dart
+++ lib/data/repository/links_repository.dart
@@ -1,6 +1,8 @@
+import 'package:flutter/material.dart';
 import 'package:linkmark_app/data/model/link.dart';
 import 'package:linkmark_app/data/model/result.dart';
 
 abstract class LinksRepository {
   Future<Result<Map<String, Link>>> getLinks();
+  Future<void> createLink({@required String url});
 }
