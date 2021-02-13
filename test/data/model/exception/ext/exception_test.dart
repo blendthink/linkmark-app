9b1b39d2ed1fc4ca0544eec50a0481f6196b9786
blendthink <info@blendthink.dev>
2021-02-13 12:29:05 +0900

    test: add UnexpectedCallException in ExceptionExt Test

--- test/data/model/exception/ext/exception_test.dart
+++ test/data/model/exception/ext/exception_test.dart
@@ -2,12 +2,19 @@ import 'package:dio/dio.dart';
 import 'package:linkmark_app/data/model/exception/ext/exception.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:linkmark_app/data/model/exception/unexpected/network_exception.dart';
+import 'package:linkmark_app/data/model/exception/unexpected/unexpected_call_exception.dart';
 import 'package:linkmark_app/data/model/exception/unexpected/unknown_exception.dart';
 
 void main() {
   group('ExceptionExt Test', () {
-    test('AppException is same', () {
+    test('UnknownException is same', () {
       const exception = UnknownException();
+      final appException = exception.toAppException();
+      expect(appException, exception);
+    });
+
+    test('UnexpectedCallException is same', () {
+      const exception = UnexpectedCallException();
       final appException = exception.toAppException();
       expect(appException, exception);
     });
