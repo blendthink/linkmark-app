cbd20c60843ddec252ccc228ff1aa0e3c94a78df
blendthink <info@blendthink.dev>
2021-02-13 12:07:37 +0900

    test: add UnknownException, UnexpectedCallException Test

---
+++ test/data/model/exception/unexpected/unexpected_call_exception_test.dart
@@ -0,0 +1,10 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:linkmark_app/data/model/exception/unexpected/unexpected_call_exception.dart';
+
+void main() {
+  test('UnexpectedCallException Test', () {
+    UnexpectedCallException();
+  });
+}
---
+++ test/data/model/exception/unexpected/unknown_exception_test.dart
@@ -0,0 +1,10 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:linkmark_app/data/model/exception/unexpected/unknown_exception.dart';
+
+void main() {
+  test('UnknownException Test', () {
+    UnknownException();
+  });
+}
